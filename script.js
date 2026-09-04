7b9c940d6986fa44b2672751c83ac36a1085df6e
Sacha <guardiandelmonte1@gmail.com>
2026-09-04 13:12:08 -0300

    Filtro
    ACTUALIZACION mascuscuas malas

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-function filtroBusqueda () {}  
+document.getElementById('search-input').addEventListener('input', filtroBusqueda);
+
+function filtroBusqueda() {
+    const texto = document.getElementById('search-input').value.toLowerCase();
+    const tarjetas = document.querySelectorAll('.card');
+
+    tarjetas.forEach(tarjeta => {
+        if (filtroAnimalRaza(tarjeta, texto)) {
+            tarjeta.style.display = "block";
+        } else {
+            tarjeta.style.display = "none";
+        }
+    });
+}
+
+function filtroAnimalRaza(elemento, busqueda) {
+    if (busqueda === "") return true;
+    const nombre = elemento.getAttribute('data-animal').toLowerCase();
+    return nombre.includes(busqueda);
+}
